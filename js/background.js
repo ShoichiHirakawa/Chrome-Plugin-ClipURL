5165d583252f6ba433f0c401c5302cb2837dac54
Shoichi Hirakawa <rel124c41ergo@gmail.com>
2018-12-25 08:29:07 +0900

    background用のソースと設定を追加

---
+++ js/background.js
@@ -0,0 +1,12 @@
+(function() {
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        if (request.text == "code-block") {
+            const selection = window.getSelection().toString().replace(/\n/g, "<br/>");
+            const insertContent = `<samp>${selection}</samp>`;
+            document.execCommand("insertHTML", false, insertContent);
+            sendResponse({ "text": insertContent });
+        }
+    });
+
+  }).call(this);
