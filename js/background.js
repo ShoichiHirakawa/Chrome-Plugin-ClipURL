565d59d718283ee9581029013a986614bb601d19
Shoichi Hirakawa <rel124c41ergo@gmail.com>
2019-01-08 01:02:31 +0900

    htmlを表示させる必要は無いので、backgroundにて処理を実行するように変更

--- js/background.js
+++ js/background.js
@@ -1,8 +1,8 @@
-function copy(tab) {
+chrome.browserAction.onClicked.addListener(function (tab) {
     var textArea = document.createElement('textarea');
     textArea.value = tab.title + "\n" + tab.url;
     document.body.appendChild(textArea);
     textArea.select();
     document.execCommand('copy');
     document.body.removeChild(textArea);
-};
+});
